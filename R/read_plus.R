b217cda52c3f6f3a1d6f591169c21f7c099adb28
Peter T. Rühr <peter.ruehr@gmail.com>
2024-09-17 18:12:38 +0200

    Delete R directory

--- R/read_plus.R
+++
@@ -1,6 +0,0 @@
-# read multiple csv files and store their filename in column
-read_plus <- function(filename) {
-  read_csv(filename,
-           show_col_types = FALSE) %>% 
-    mutate(filename = gsub("\\.csv", "", basename(filename)))
-}
